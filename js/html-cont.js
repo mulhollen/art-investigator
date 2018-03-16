5dcafeb7cf6a879d0535106f0e1854a0e22524dc
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-03-16 14:54:19 -0500

    screwed up my branch. user auth is done. base html is set. sign-in html is set. working on home html

---
+++ js/html-cont.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let signInPage = `<div class="sign-in column text-center align-middle align-middle h-100">    
+                        <h3>Sign in with:</h3>    
+                        <a id="login">
+                            <img class="round" src="img/sign-in/google-logo.png">
+                        </a>
+                    </div>`;

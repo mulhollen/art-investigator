3716d5a009c3223639defe8ae982d549effae7e6
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-03-28 13:16:44 -0500

    change img paths

--- js/questions.js
+++ js/questions.js
@@ -4,57 +4,57 @@ var questionArray =
     [
         {
             q: "A Donald Duck figure?",
-            hint: "../img/hints/q_01.png",
+            hint: "./img/hints/q_01.png",
             id: "q_01"
         },
         {
             q: "Some sock monkeys lounging around?",
-            hint: "../img/hints/q_04.png",
+            hint: "./img/hints/q_04.png",
             id: "q_02"
         },
         {
             q: "Some rainbow socks living a new life?",
-            hint: "../img/hints/q_11.png",
+            hint: "./img/hints/q_11.png",
             id: "q_03"
         },
         {
             q: "An easter bunny way up high?",
-            hint: "../img/hints/q_10.png",
+            hint: "./img/hints/q_10.png",
             id: "q_04"
         },
         {
             q: "A person dancing that sounds like waves?",
-            hint: "../img/hints/q_02.png",
+            hint: "./img/hints/q_02.png",
             id: "q_05"
         },
         {
             q: "A leopard hiding in some beads?",
-            hint: "../img/hints/q_03.png",
+            hint: "./img/hints/q_03.png",
             id: "q_06"
         },
         {
             q: "Some rainbow waves?",
-            hint: "../img/hints/q_08.png",
+            hint: "./img/hints/q_08.png",
             id: "q_07"
         },
         {
             q: "A kitten covered in some jewels?",
-            hint: "../img/hints/q_05.png",
+            hint: "./img/hints/q_05.png",
             id: "q_08"
         },
         {
             q: "A bird, or two, that has a friend?",
-            hint: "../img/hints/q_07.png",
+            hint: "./img/hints/q_07.png",
             id: "q_09"
         },
         {
             q: "A bit of blanket hiding behind a pair?",
-            hint: "../img/hints/q_08.png",
+            hint: "./img/hints/q_08.png",
             id: "q_10"
         },
         {
             q: "8 spotted socks?",
-            hint: "../img/hints/q_06.png",
+            hint: "./img/hints/q_06.png",
             id: "q_11"
         },
 
